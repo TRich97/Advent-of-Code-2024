3765a2722e38b8f32537aa2d1568215b79848883
Tomara <efctom@googlemail.com>
2024-12-01 19:02:42 +0000

    Add project files.

---
+++ Advent of Code 2024/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_Code_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
